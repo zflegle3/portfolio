acc0f724e6182463b10cfcb4902b76c196a4c120
Zachary Flegle <zflegle3@gmail.com>
2022-09-01 16:53:06 -0400

    Added basic and interactive cloud styling

--- src/components/Clouds.js
+++ src/components/Clouds.js
@@ -2,9 +2,23 @@ import { v4 as uuidv4 } from 'uuid';
 
 function Clouds() {
 
+
+
+    const hideCloud = (e) => {
+        console.log(e.target)
+        e.target.className = "cloud hide"
+    }
+
+    const showCloud = (e) => {
+        console.log(e.target)
+        setTimeout(() => {
+            e.target.className = "cloud"
+          }, 3000);
+    }
+
     let cloudCoords=[];
     for (let i=0; i<10; i++) {//left coord
-        for (let j=0; j<6; j++) {//top coord
+        for (let j=0; j<7; j++) {//top coord
             let coords ={
                 left: i,
                 top: j,
@@ -14,7 +28,7 @@ function Clouds() {
     }
     console.log(cloudCoords);
     let cloudsAll = cloudCoords.map((cloud) => 
-        <div key={uuidv4()} className="cloud" style={{color:"blue",top:`${cloud}`}}>Cloud</div>
+        <div key={uuidv4()} className="cloud" onMouseEnter={hideCloud} onMouseLeave={showCloud}  style={{color:"blue",top:`${(cloud.top*10)}%`,left:`${(cloud.left*10)}%`}}></div>
     );
 
     // `top:${cloud.top}%;left:${cloud.left}%`
@@ -23,7 +37,6 @@ function Clouds() {
 
   return (
     <div className="clouds">
-        <p>Clouds</p>
         {cloudsAll}
     </div>
   );
